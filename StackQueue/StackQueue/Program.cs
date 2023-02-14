91ca1f065e8ffc1d1fa758d7270fb3f17deed74d
Prashant <prashant.chaurasiya.cp@gmail.com>
2023-02-14 23:57:38 +0530

    [Prashant]Add: Ability to create a stack of 56 30 70

---
+++ StackQueue/StackQueue/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace StackQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------StackQueue-------------");
+            Console.WriteLine(" 1.Push ");
+            Console.WriteLine("Enter the option");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Stack stack = new Stack();
+            switch (num)
+            {
+                case 1:
+                    stack.Push(70);
+                    stack.Push(30);
+                    stack.Push(56);
+                    stack.Display();
+                    break;
+                default:
+                    Console.WriteLine("Enter a valid option");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
